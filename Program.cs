3ff26f1b13217b0bd9232f24ee5d4e5c1bea326d
«Oksana <oxana.shishkova@gmail.com>
2022-12-19 15:06:49 +0300

    Start commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// №6 На вход принимает число N, на выходе выдает четные числа от 1 до N
+
+
+Console.WriteLine("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+int i = 1;
+
+while (i <= number)
+{
+    if (i % 2 == 0) { Console.Write($"{i} "); }
+    i++;
+}
+
+
